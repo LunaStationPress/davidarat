9695115169f6b9e4eeefd50c0eb60cd4f4b56aa6
Jennifer Parsons <jenniferlynparsons@gmail.com>
2014-09-28 11:31:48 -0400

    first iteration with World of David

--- php/send_form_email.php
+++ php/send_form_email.php
@@ -1,11 +1,11 @@
 <?php
 if(isset($_POST['email'])) {
-     
+
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "lazarevacharmonika@gmail.com";
+    $email_to = "littleoracle@gmail.com";
     $email_subject = "Your email subject line";
-     
-     
+
+
     function died($error) {
         // your error code can go here
         echo "<h3>We are very sorry, but there were error(s) found with the form you submitted.</h3>";
@@ -14,22 +14,22 @@ if(isset($_POST['email'])) {
         echo "Please go back and fix these errors.<br /><br />";
         die();
     }
-     
+
     // validation expected data exists
     if(!isset($_POST['first_name']) ||
         !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['website']) ||
         !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
     }
-     
+
     $first_name = $_POST['first_name']; // required
     $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
     $website = $_POST['website']; // not required
     $comments = $_POST['comments']; // required
-     
+
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
   if(!preg_match($email_exp,$email_from)) {
@@ -49,26 +49,26 @@ if(isset($_POST['email'])) {
     died($error_message);
   }
     $email_message = "Form details below.\n\n";
-     
+
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
-     
+
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "website: ".clean_string($website)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
-     
-     
+
+
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+@mail($email_to, $email_subject, $email_message, $headers);
 ?>
- 
+
 <!-- include your own success html here -->
 <html>
 <head>
@@ -117,9 +117,9 @@ Thank you for contacting us. We will be in touch with you very soon.
 </h1>
 </div>
 <div class="info">
-If your browser doesn't automatically go to home page within a few seconds, 
-you may want to go to 
-<a href="./index.html">home page</a> 
+If your browser doesn't automatically go to home page within a few seconds,
+you may want to go to
+<a href="./index.html">home page</a>
 manually.
 </div>
 </body>
